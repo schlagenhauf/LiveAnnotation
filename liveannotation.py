8e75508ff34348f5ed6bba8ff10d340c190e2531
Jonas Schlagenhauf <jonas.schlagenhauf@jupiter.uni-freiburg.de>
2015-10-21 11:47:43 +0200

    Add parameter tree as config ui

--- liveannotation.py
+++ liveannotation.py
@@ -5,6 +5,9 @@ from PyQt4 import QtCore, QtGui, uic
 import pyqtgraph as pg
 from pyqtgraph.ptime import time
 from pyqtgraph.parametertree import Parameter, ParameterTree, ParameterItem, registerParameterType
+import pyqtgraph.parametertree.parameterTypes as pTypes
+
+
 
 
 import numpy as np
@@ -27,6 +30,7 @@ class LiveAnnotatorWin(QtGui.QMainWindow, form_class):
         self.vid.setSource("videotestsource")
         self.plt = PlotWidget(self.plotView)
         self.plt.setSource(sys.stdin)
+        self.pmtree = ParameterTreeWidget(self.parameterView)
 
         # connect elements
         self.btnPlay.clicked.connect(self.vid.play)
@@ -115,9 +119,104 @@ class VideoWidget:
         pass
 
 
-class ParameterTree:
-    def __init__(self):
-        pass
+class ParameterTreeWidget:
+    def __init__(self, parameterView):
+        params = [
+            {'name': 'Basic parameter data types', 'type': 'group', 'children': [
+                {'name': 'Integer', 'type': 'int', 'value': 10},
+                {'name': 'Float', 'type': 'float', 'value': 10.5, 'step': 0.1},
+                {'name': 'String', 'type': 'str', 'value': "hi"},
+                {'name': 'List', 'type': 'list', 'values': [1,2,3], 'value': 2},
+                {'name': 'Named List', 'type': 'list', 'values': {"one": 1, "two": 2, "three": 3}, 'value': 2},
+                {'name': 'Boolean', 'type': 'bool', 'value': True, 'tip': "This is a checkbox"},
+                {'name': 'Color', 'type': 'color', 'value': "FF0", 'tip': "This is a color button"},
+                {'name': 'Subgroup', 'type': 'group', 'children': [
+                    {'name': 'Sub-param 1', 'type': 'int', 'value': 10},
+                    {'name': 'Sub-param 2', 'type': 'float', 'value': 1.2e6},
+                ]},
+                {'name': 'Text Parameter', 'type': 'text', 'value': 'Some text...'},
+                {'name': 'Action Parameter', 'type': 'action'},
+            ]},
+            {'name': 'Numerical Parameter Options', 'type': 'group', 'children': [
+                {'name': 'Units + SI prefix', 'type': 'float', 'value': 1.2e-6, 'step': 1e-6, 'siPrefix': True, 'suffix': 'V'},
+                {'name': 'Limits (min=7;max=15)', 'type': 'int', 'value': 11, 'limits': (7, 15), 'default': -6},
+                {'name': 'DEC stepping', 'type': 'float', 'value': 1.2e6, 'dec': True, 'step': 1, 'siPrefix': True, 'suffix': 'Hz'},
+
+            ]},
+            {'name': 'Save/Restore functionality', 'type': 'group', 'children': [
+                {'name': 'Save State', 'type': 'action'},
+                {'name': 'Restore State', 'type': 'action', 'children': [
+                    {'name': 'Add missing items', 'type': 'bool', 'value': True},
+                    {'name': 'Remove extra items', 'type': 'bool', 'value': True},
+                ]},
+            ]},
+            {'name': 'Extra Parameter Options', 'type': 'group', 'children': [
+                {'name': 'Read-only', 'type': 'float', 'value': 1.2e6, 'siPrefix': True, 'suffix': 'Hz', 'readonly': True},
+                {'name': 'Renamable', 'type': 'float', 'value': 1.2e6, 'siPrefix': True, 'suffix': 'Hz', 'renamable': True},
+                {'name': 'Removable', 'type': 'float', 'value': 1.2e6, 'siPrefix': True, 'suffix': 'Hz', 'removable': True},
+            ]},
+            ComplexParameter(name='Custom parameter group (reciprocal values)'),
+            ScalableGroup(name="Expandable Parameter Group", children=[
+                {'name': 'ScalableParam 1', 'type': 'str', 'value': "default param 1"},
+                {'name': 'ScalableParam 2', 'type': 'str', 'value': "default param 2"},
+            ]),
+        ]
+
+        self.p = Parameter.create(name='params', type='group', children=params)
+
+        self.p.param('Save/Restore functionality', 'Save State').sigActivated.connect(self.save)
+        self.p.param('Save/Restore functionality', 'Restore State').sigActivated.connect(self.restore)
+
+        self.t = parameterView # use the ID of the promoted graphicsView
+        self.t.setParameters(self.p, showTop=False)
+        self.t.show()
+        self.t.resize(400,400)
+
+
+    def save(self):
+        self.state = self.p.saveState()
+
+    def restore(self):
+        add = self.p['Save/Restore functionality', 'Restore State', 'Add missing items']
+        rem = self.p['Save/Restore functionality', 'Restore State', 'Remove extra items']
+        self.p.restoreState(self.state, addChildren=add, removeChildren=rem)
+
+
+class ComplexParameter(pTypes.GroupParameter):
+    def __init__(self, **opts):
+        opts['type'] = 'bool'
+        opts['value'] = True
+        pTypes.GroupParameter.__init__(self, **opts)
+
+        self.addChild({'name': 'A = 1/B', 'type': 'float', 'value': 7, 'suffix': 'Hz', 'siPrefix': True})
+        self.addChild({'name': 'B = 1/A', 'type': 'float', 'value': 1/7., 'suffix': 's', 'siPrefix': True})
+        self.a = self.param('A = 1/B')
+        self.b = self.param('B = 1/A')
+        self.a.sigValueChanged.connect(self.aChanged)
+        self.b.sigValueChanged.connect(self.bChanged)
+
+    def aChanged(self):
+        self.b.setValue(1.0 / self.a.value(), blockSignal=self.bChanged)
+
+    def bChanged(self):
+        self.a.setValue(1.0 / self.b.value(), blockSignal=self.aChanged)
+
+
+class ScalableGroup(pTypes.GroupParameter):
+    def __init__(self, **opts):
+        opts['type'] = 'group'
+        opts['addText'] = "Add"
+        opts['addList'] = ['str', 'float', 'int']
+        pTypes.GroupParameter.__init__(self, **opts)
+
+    def addNew(self, typ):
+        val = {
+            'str': '',
+            'float': 0.0,
+            'int': 0
+        }[typ]
+        self.addChild(dict(name="ScalableParam %d" % (len(self.childs)+1), type=typ, value=val, removable=True, renamable=True))
+
 
 
 class PlotWidget:
@@ -141,6 +240,7 @@ class PlotWidget:
 
     def update(self):
         # get line from pipe
+        """
         line = sys.stdin.readline()
         if line:
             # read space separated floats
@@ -153,6 +253,7 @@ class PlotWidget:
             self.curve.setData(np.array(self.data))
 
             app.processEvents()  ## force complete redraw for every plot
+        """
 
 
     def setSource(self, source):
