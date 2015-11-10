bec387b7fe826a89f2e63d3b43cb763c4b0a97e7
Jonas <jonas.schlagenhauf@gmx.de>
2015-11-10 17:39:50 +0100

    Implement actual reaction to key presses

---
+++ dataparser.py
@@ -0,0 +1,44 @@
+import sys, select
+from PyQt4 import QtCore, QtGui
+
+class Signal(QtCore.QObject):
+    newData = QtCore.pyqtSignal(tuple)
+
+signal = Signal()
+source = sys.stdin
+timer = None
+
+
+## Creates a QTimer that polls the source for data
+def start(period):
+    global timer
+    timer = QtCore.QTimer()
+    timer.timeout.connect(processData)
+    timer.start(period)
+
+
+## Stop polling for data
+def stop():
+    timer = []
+
+
+# get all available data from the source and emit signals
+def processData():
+    # get data
+    while True:
+        line = source.readline()
+        if line:
+            # read space separated data fields
+            fields = line.split(' ')
+            nums = [float(i) for i in fields[1:]]
+            data = (fields[0], nums)
+
+            # emit signal
+            signal.newData.emit(data)
+        else:
+            break
+
+
+## Connect function to signal
+def connect(callback):
+    signal.newData.connect(callback)
