ad68ee7816b72789ae69a61039a35814ee73b20e
Jonas Schlagenhauf <jonas.schlagenhauf@jupiter.uni-freiburg.de>
2015-12-01 14:19:30 +0100

    Annotator creates labels itself now

--- liveannotation.py
+++ liveannotation.py
@@ -60,6 +60,7 @@ class LiveAnnotation(QtGui.QMainWindow, main_form):
     dp.stop()
     self.annotatorConfig.quit()
     self.plotter.quit()
+    self.annotator.quit()
 
     event.accept()
 
@@ -89,130 +90,124 @@ class PlotLabel:
 ## Widget managing plotting
 class GraphicsLayoutWidget:
   ## Constructor
-    def __init__(self, widget):
-      # create plot window
-        self.w = widget
-        self.plots = []
+  def __init__(self, widget): # create plot window self.w = widget
+    self.plots = []
+    self.w = widget
 
-        self.yLabels = [] # names of each sensor dimension
-        self.classLabels = [] # list of plotlabel containers
-        self.data = np.zeros((0,0)) # a matrix containing data for each dimension per row
+    self.yLabels = [] # names of each sensor dimension
+    self.classLabels = [] # list of plotlabel containers
+    self.data = np.zeros((0,0)) # a matrix containing data for each dimension per row
 
-        self.statusLabel = self.w.parent().findChild(QtGui.QLabel, "labelPlotStatus")
+    self.statusLabel = self.w.parent().findChild(QtGui.QLabel, "labelPlotStatus")
 
-        # config
-        self.xLimit = 300
-        self.rate = 50
-
-
-    def update(self):
-      numSamples = self.data.shape[1]
-
-      self.__updateNumberOfPlots()
-
-      for i,pl in enumerate(self.plots):
-        pl.listDataItems()[0].setData(self.data[i,:])
-        if self.xLimit < numSamples:
-          pl.setXRange(numSamples - self.xLimit, numSamples)
-
-      self.__updateClassLabels()
-
-      app.processEvents()  ## force complete redraw for every plot
+    # config
+    self.xLimit = 300
+    self.rate = 50
 
 
-    def quit(self):
-      for p in self.plots:
-        self.w.removeItem(p)
+  def update(self):
+    numSamples = self.data.shape[1]
+
+    self.__updateNumberOfPlots()
+
+    for i,pl in enumerate(self.plots):
+      pl.listDataItems()[0].setData(self.data[i,:])
+      if self.xLimit < numSamples:
+        pl.setXRange(numSamples - self.xLimit, numSamples)
+
+    self.__updateClassLabels()
+
+    app.processEvents()  ## force complete redraw for every plot
+
+  def quit(self):
+    for p in self.plots:
+      self.w.removeItem(p)
 
 
 
-    ## creates new linearRegion objects if necessary and deletes outdated ones
-    def __updateClassLabels(self):
-      # update class labeling
-        for cl in self.classLabels:
-          if not cl.linReg:
-            for pl in self.plots:
-              linReg = pg.LinearRegionItem([cl.startIdx, cl.endIdx])
-              linReg.setZValue(-10)
-              pl.addItem(linReg)
-              cl.linReg.append(linReg)
+  ## creates new linearRegion objects if necessary and deletes outdated ones
+  def __updateClassLabels(self):
+    # update class labeling
+      for cl in self.classLabels:
+        if not cl.linReg:
+          for pl in self.plots:
+            linReg = pg.LinearRegionItem([cl.startIdx, cl.endIdx])
+            linReg.setZValue(-10)
+            pl.addItem(linReg)
+            cl.linReg.append(linReg)
 
-          # update bounds if necessary
-          if [cl.startIdx, cl.endIdx] != cl.linReg[0].getRegion():
-            endIdx = self.data.shape[1] if cl.endIdx == -1 else cl.endIdx
-            for lr in cl.linReg:
-              lr.setRegion([cl.startIdx, endIdx])
+        # update bounds if necessary
+        if [cl.startIdx, cl.endIdx] != cl.linReg[0].getRegion():
+          endIdx = self.data.shape[1] if cl.endIdx == -1 else cl.endIdx
+          for lr in cl.linReg:
+            lr.setRegion([cl.startIdx, endIdx])
 
-        self.__setStatusLabel()
-
-
-
-    def __setStatusLabel(self):
-      numSamples = self.data.shape[1]
-      visibleSamples = self.xLimit if numSamples > self.xLimit else numSamples
-      self.statusLabel.setText('Samples: ' + str(numSamples) + ' [' + str(visibleSamples) + ' visible]')
+      self.__setStatusLabel()
 
 
 
-    def setYLabels(self, labels):
-      self.labels = labels
-      self.__updateYLabels()
-
-
-    ## slot for appending new data
-    @pyqtSlot(tuple)
-    def dataSlot(self, data):
-      ndata = np.array(data[1], ndmin=2).T
-
-      ## check if length of data vector has changed, and pad with zeros if necessary
-      if ndata.shape[0] > self.data.shape[0]:
-        self.data = np.vstack((self.data, np.zeros((ndata.shape[0] - self.data.shape[0], self.data.shape[1]))))
-      elif ndata.shape[0] < self.data.shape[0]:
-        ndata = np.vstack((ndata, np.zeros((self.data.shape[0] - ndata.shape[0], 1))))
-
-      # append
-      self.data = np.hstack((self.data, ndata))
-
-      self.update()
-
-
-    ## slot for reacting to newly annotated labels
-    @pyqtSlot(tuple)
-    def onNewClassLabel(self, data):
-      # start a new label area or end a started one
-      if data[1]: # label start
-        self.classLabels.append(PlotLabel(data[0], data[2], -1)) # create new label that is open to the right
-
-      else: # label end
-        # find start of label
-        for l in reversed(self.classLabels):
-          if l.name == data[0]: # if same label type
-            l.endIdx = data[2]
-            break
-
-        else:
-          print "Error: ending label failed, no corresponding start"
-
-      for l in self.classLabels:
-        print l
+  def __setStatusLabel(self):
+    numSamples = self.data.shape[1]
+    visibleSamples = self.xLimit if numSamples > self.xLimit else numSamples
+    self.statusLabel.setText('Samples: ' + str(numSamples) + ' [' + str(visibleSamples) + ' visible]')
 
 
 
-    def __updateYLabels(self):
-      # assign y axis labels (if more/less labels are given, list is truncated accordingly)
-        for p,l in zip(self.plots, self.yLabels):
-          p.setLabel('left', l)
+  def setYLabels(self, labels):
+    self.labels = labels
+    self.__updateYLabels()
 
 
-    def __updateNumberOfPlots(self):
-      numDims = self.data.shape[0]
-      while len(self.plots) < numDims:
-        self.plots.append(self.w.addPlot())
-        self.plots[-1].plot()
-        self.plots[-1].showGrid(True, True)
-        self.w.nextRow()
+  ## slot for appending new data
+  @pyqtSlot(tuple)
+  def dataSlot(self, data):
+    ndata = np.array(data[1], ndmin=2).T
 
-      self.__updateYLabels()
+    ## check if length of data vector has changed, and pad with zeros if necessary
+    if ndata.shape[0] > self.data.shape[0]:
+      self.data = np.vstack((self.data, np.zeros((ndata.shape[0] - self.data.shape[0], self.data.shape[1]))))
+    elif ndata.shape[0] < self.data.shape[0]:
+      ndata = np.vstack((ndata, np.zeros((self.data.shape[0] - ndata.shape[0], 1))))
+
+    # append
+    self.data = np.hstack((self.data, ndata))
+
+    self.update()
+
+
+  ## slot for reacting to newly annotated labels
+  @pyqtSlot(tuple) # tuple = (name, start or end, index)
+  def onNewClassLabel(self, data):
+    # start a new label area or end a started one
+    if data[1]: # label start
+      self.classLabels.append(PlotLabel(data[0], data[2], -1)) # create new label that is open to the right
+
+    else: # label end
+      # find start of label
+      for l in reversed(self.classLabels):
+        if l.name == data[0]: # if same label type
+          l.endIdx = data[2]
+          break
+
+      else:
+        print "Error: ending label failed, no corresponding start"
+
+
+  def __updateYLabels(self):
+    # assign y axis labels (if more/less labels are given, list is truncated accordingly)
+      for p,l in zip(self.plots, self.yLabels):
+        p.setLabel('left', l)
+
+
+  def __updateNumberOfPlots(self):
+    numDims = self.data.shape[0]
+    while len(self.plots) < numDims:
+      self.plots.append(self.w.addPlot())
+      self.plots[-1].plot()
+      self.plots[-1].showGrid(True, True)
+      self.w.nextRow()
+
+    self.__updateYLabels()
 
 
 
@@ -332,7 +327,6 @@ class ParameterTreeWidget:
     self.t = parameterView # use the ID of the promoted graphicsView
     self.t.setParameters(self.p, showTop=False)
     self.t.show()
-    print "bla"
 
 
   ## recursively go through the tree and search for a parameter with <key>
@@ -450,50 +444,40 @@ class AnnotationConfigWidget(QtCore.QObject):
 
       self.saveShortcutsOnExit = True
 
+
     def quit(self):
       if self.saveShortcutsOnExit:
         # serialize config
-        print "Saving annotatorConfig"
         f = open("shortcuts.cfg", "wb")
         pickle.dump(self.annotatorConfig, f, pickle.HIGHEST_PROTOCOL)
-
-
-    ## Sets the displayed configuration by a list of LabelMeta instances
-    def setConfig(self, cfg):
-      pass
-
-
-    ## Returns the (modified) configuration as a list of LabelMeta instances
-    def getConfig(self):
-      pass
 
 
     ## Synchronizes the internal list with the displayed table
     def syncLists(self):
       # save sort column and order for sorting afterwards
-        sortCol = self.tableWidget.horizontalHeader().sortIndicatorSection()
-        sortOrd = self.tableWidget.horizontalHeader().sortIndicatorOrder()
+      sortCol = self.tableWidget.horizontalHeader().sortIndicatorSection()
+      sortOrd = self.tableWidget.horizontalHeader().sortIndicatorOrder()
 
-        # clear table and reinsert all items
-        self.tableWidget.clearContents()
-        self.tableWidget.setRowCount(len(self.annotatorConfig))
-        for i,tup in enumerate(self.annotatorConfig.itervalues()):
-            v = tup[0]
-            self.tableWidget.setItem(i,0,QtGui.QTableWidgetItem(v.name))
-            self.tableWidget.setItem(i,1,QtGui.QTableWidgetItem(v.key.toString()))
-            if v.toggleMode:
-                self.tableWidget.setItem(i,2,QtGui.QTableWidgetItem("Toggle"))
-            else:
-                self.tableWidget.setItem(i,2,QtGui.QTableWidgetItem("Hold"))
-            self.tableWidget.setItem(i,3,QtGui.QTableWidgetItem(v.description))
+      # clear table and reinsert all items
+      self.tableWidget.clearContents()
+      self.tableWidget.setRowCount(len(self.annotatorConfig))
+      for i,tup in enumerate(self.annotatorConfig.itervalues()):
+        v = tup[0]
+        self.tableWidget.setItem(i,0,QtGui.QTableWidgetItem(v.name))
+        self.tableWidget.setItem(i,1,QtGui.QTableWidgetItem(v.key.toString()))
+        if v.toggleMode:
+          self.tableWidget.setItem(i,2,QtGui.QTableWidgetItem("Toggle"))
+        else:
+          self.tableWidget.setItem(i,2,QtGui.QTableWidgetItem("Hold"))
+        self.tableWidget.setItem(i,3,QtGui.QTableWidgetItem(v.description))
 
-        # sort table again
-        self.tableWidget.sortItems(sortCol, sortOrd)
+      # sort table again
+      self.tableWidget.sortItems(sortCol, sortOrd)
 
-        # update key map
-        self.shortcuts = []
-        for v,s in self.annotatorConfig.itervalues():
-            self.shortcuts.append(QtGui.QShortcut(v.key, self.widget, lambda: self.__onShortcutEnable(v.key)))
+      # update key map
+      self.shortcuts = []
+      for v,s in self.annotatorConfig.itervalues():
+        self.shortcuts.append(QtGui.QShortcut(v.key, self.widget, lambda: self.__onShortcutEnable(v.key)))
 
 
     ## pseudo slot for key presses. translates key press into label and status
@@ -511,7 +495,6 @@ class AnnotationConfigWidget(QtCore.QObject):
         dialog.setModal(True)
         if dialog.exec_(): # if dialog closes with accept()
             lm = dialog.lm
-            #print lm.name + ' ' + lm.key + ' ' + lm.description + ' ' + str(lm.toggleMode)
             if self.annotatorConfig.has_key(lm.key):
               self.annotatorConfig[lm.name] = (lm, self.annotatorConfig[lm.name][1])
             else:
@@ -554,11 +537,20 @@ class AnnotationConfigWidget(QtCore.QObject):
 ## Container for label information
 class LabelMeta:
   ## Constructor
-    def __init__(self, name = "", key = None, description = "", toggleMode = True):
-      self.name = name
-      self.key = key
-      self.description = description
-      self.toggleMode = toggleMode
+  def __init__(self, name = "", key = None, description = "", toggleMode = True):
+    self.name = name
+    self.key = key
+    self.description = description
+    self.toggleMode = toggleMode
+
+
+## Container for actual labels
+class Label:
+  ## Constructor
+  def __init__(self, name = "", startIdx = 0, endIdx = -1):
+    self.name = name
+    self.startIdx = startIdx
+    self.endIdx = endIdx
 
 
 ## A tool for annotating a stream of sensor data with labels
@@ -574,14 +566,9 @@ class Annotator(QtCore.QObject):
     self.outFilePath = 'annotationOut.txt'
 
 
-  def __del__(self):
+  def quit(self):
     # write annotation data
     self.writeAnnotatedData()
-
-
-  def connectSignals(self, annotatorConfig):
-    # set slot for key presses
-    annotatorConfig.keyPressSignal.connect(self.__onShortcutEnable)
 
 
   ## Slot for receiving data
@@ -600,31 +587,51 @@ class Annotator(QtCore.QObject):
 
 
   ## Slot for key presses
-  @pyqtSlot(tuple)
+  @pyqtSlot(tuple) # tuple = (name, on / off)
   def onShortcutEnable(self, data):
     # trigger corresponding label
-    if self.data.shape[1] != 0:
-      anno = (data[0], data[1], self.data.shape[1])
-      self.annotations.append(anno)
-      self.newLabelSignal.emit(anno)
-      print 'New label: ' + str(anno)
-
-    else:
+    if self.data.shape[1] == 0:
       print 'Error: No sensor data!'
+      return
+
+    # if it is an closing edge, find the corresponding start
+
+    anno = (data[0], data[1], self.data.shape[1])
+    self.annotations.append(anno)
+    self.newLabelSignal.emit(anno) # tuple = (name, start or end, index)
+    print 'New label: ' + str(anno)
+
+    # start a new label area or end a started one
+    if data[1]: # label start
+      self.annotations.append(PlotLabel(data[0], data[2], -1)) # create new label that is open to the right
+
+    else: # label end
+      # find start of label
+      for l in reversed(self.classLabels):
+        if l.name == data[0]: # if same label type
+          l.endIdx = data[2]
+          break
+
+      else:
+        print "Error: ending label failed, no corresponding start"
+
+
 
 
   def writeAnnotatedData(self):
-    print "annotation is written"
     if not self.annotations:
       return
+
+
+    print "annotation is written"
 
     # create "empty" labels
     outLabels = ["other" for i in range(self.data.shape[1])]
 
     # apply annotation
     for a in self.annotations:
-      for i in range(a.startIdx, a.endIdx):
-        outLabels[i] = a.name
+      for i in range(a[1], a[2]):
+        outLabels[i] = a[0]
 
     # create final output data
     outData = ""
